1610865ad9ddebc8f4f69b5b01e18e09ea365b2b
Tristan Poveda <povedatristan@gmail.com>
2024-12-11 15:04:52 +0100

    Feat : Inventory icon UI

--- Assets/App/Scripts/Runtime/Wrapper/RSO/RSO_InventoryContent.cs
+++
@@ -1,4 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "RSO_InventoryContent", menuName = "RSO/RSO_InventoryContent")]
-public class RSO_InventoryContent : BT.ScriptablesObject.RuntimeScriptableObject<InventoryMachineData>{}
