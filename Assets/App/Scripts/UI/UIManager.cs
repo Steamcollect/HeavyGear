8c95c2726a1f027396037573bd68efa5dc9a3a7c
Tristan Poveda <povedatristan@gmail.com>
2024-12-11 13:21:30 +0100

    Feat : Update ui

--- Assets/App/Scripts/UI/UIManager.cs
+++ Assets/App/Scripts/UI/UIManager.cs
@@ -1,27 +1,36 @@
+using TMPro;
 using UnityEngine;
 public class UIManager : MonoBehaviour
 {
     [Header("Settings")]
-
+    [SerializeField] private TMP_Text _coinText;
 
     [Header("References")]
+    [Space(10)]
     [SerializeField] private RSO_Coins _rsoCoin;
-
-    //[Space(10)]
-    // RSO
     // RSF
     // RSP
 
     //[Header("Input")]
     //[Header("Output")]
 
+    private void Awake()
+    {
+        UpdateCoinUI();
+    }
+
     private void OnEnable()
     {
         _rsoCoin.OnChanged += UpdateCoinUI;
     }
 
+    private void OnDisable()
+    {
+        _rsoCoin.OnChanged -= UpdateCoinUI;
+    }
+
     private void UpdateCoinUI()
     {
-
+        _coinText.text = _rsoCoin.Value.ToString();
     }
 }
