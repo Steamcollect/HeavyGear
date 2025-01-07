ba9e2e2a21f85732f0ddde74d6c0956a65066a4c
Tristan Poveda <povedatristan@gmail.com>
2025-01-07 21:18:35 +0100

    Fix Canvas All Scene + Shop/Inventory

--- Assets/App/Scripts/Runtime/UISystem/CardUI.cs
+++ Assets/App/Scripts/Runtime/UISystem/CardUI.cs
@@ -10,15 +10,14 @@ public class CardUI : MonoBehaviour
     [SerializeField] private MachineRarity machineRarity;
     [Space(10)]
     [SerializeField] private Image background;
-    [Space(10)]
     [SerializeField] private Image border;
-    [SerializeField] private Image borderGlow;
-    [SerializeField] private Image borderShadow;
-    [Space(10)]
-    [SerializeField] private Image sliderBackground;
-    [SerializeField] private Image sliderFill;
     [Space(10)]
     [SerializeField] private TextMeshProUGUI cardNumber;
+    [SerializeField] private TextMeshProUGUI cardLevel;
+    [SerializeField] private TextMeshProUGUI cardName;
+    [Space(10)]
+    [SerializeField] private Image cardIcon;
+    [Space(10)]
     [SerializeField] private Slider slider;
 
     [Header("References")]
@@ -43,18 +42,18 @@ public class CardUI : MonoBehaviour
                 Debug.LogWarning("No machine rarity set");
                 break;
         }
-
+        cardLevel.text = machine.machineLevel.ToString();
+        cardName.text = machine.machine.machineName.ToString();
         cardNumber.text = machine.amountAquired + "/" + machine.maxAmount;
+
         slider.value = (float)machine.amountAquired / (float)machine.maxAmount;
+
+        cardIcon.sprite = machine.machine.machineVisual;
     }
 
     private void SetCardVisual(SSO_CardVisual cardVisual)
     {
         background.color = cardVisual.firstColor;
-        border.color = cardVisual.firstColor;
-        borderGlow.color = cardVisual.secondColor;
-        borderShadow.color = cardVisual.firstColor;
-        sliderBackground.color = cardVisual.firstColor;
-        sliderFill.color = cardVisual.firstColor;
+        border.color = cardVisual.secondColor;
     }
 }
